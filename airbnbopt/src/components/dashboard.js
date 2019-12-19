98678246634125d1a619ad1fc0aa2b29452f23b0
Jared Brown <jared@Jareds-MBP.lan1>
2019-12-18 21:42:51 -0800

    initial commit

--- airbnbopt/src/components/dashboard.js
+++ airbnbopt/src/components/dashboard.js
@@ -1,47 +1,45 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import styled from "styled-components";
 import DashboardCard from "./DashboardCard";
 
 const Content = styled.div`
-    display: flex;
-    justify-content: center;
-    flex-direction: row-reverse;
-`
+  display: flex;
+  justify-content: center;
+  flex-direction: row-reverse;
+`;
 
 export default Dashboard => {
+  const [,] = useState([]);
 
-const [,] = useState([]);    
-
-useEffect(() => {
+  useEffect(() => {
     axios
-        .get("")
-        .then (response =>{
-         console.log(response);
-    })
-         .catch (error => {
-         console.log(error);
-    });
-}, []);
+      .get("")
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
 
-    
-    return (
-        <Content className='content'>
-            <div>
-                <div>
-                    <h2>Content here</h2>
-                    <h3>Hello, *users name here*</h3>
-                    <h4>Popular Homes this Week</h4>
-                </div>
-            </div>
-            <div>
-                <h3>Filter Results</h3>
-                <ul>
-                    <li>Rooms</li> //dropdown here
-                    <li>Bathrooms</li> //dropdown here
-                    <li>Pool</li> //checkbox
-                </ul>
-            </div>
-        </Content>
-    )
-}
+  return (
+    <Content className="content">
+      <div>
+        <div>
+          <h2>Content here</h2>
+          <h3>Hello, *users name here*</h3>
+          <h4>Popular Homes this Week</h4>
+        </div>
+      </div>
+      <div>
+        <h3>Filter Results</h3>
+        <ul>
+          <li>Rooms</li> //dropdown here
+          <li>Bathrooms</li> //dropdown here
+          <li>Pool</li> //checkbox
+        </ul>
+      </div>
+    </Content>
+  );
+};
