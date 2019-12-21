2b47311037848d6f034761cae5c2d356f01a78a6
Jared Brown <jared@Jareds-MBP.lan1>
2019-12-21 10:51:55 -0800

    added 2nd and 3rd house for default data

--- airbnbopt/src/components/DashboardCard.js
+++ airbnbopt/src/components/DashboardCard.js
@@ -1,11 +1,15 @@
 import React from "react";
 import styled from "styled-components";
 import image1 from "../images/Airbnb1.jpg";
+import image2 from "../images/Airbnb2.jpg";
+import image3 from "../images/Airbnb3.jpg";
+import image4 from "../images/Airbnb4.jpg";
+import image5 from "../images/Airbnb5.jpg";
 
 const Container = styled.div`
   height: 200px;
   width: 550px;
-  border: solid 2px;
+  border: solid 1px;
   display: flex;
   justify-content: center;
 `;
@@ -16,7 +20,7 @@ const ImageCont = styled.div`
 `;
 
 const Image = styled.div`
-  height: 180px;
+  height: 220px;
   width: 255px;
 `;
 
@@ -61,23 +65,44 @@ const DashboardCard = props => {
       <Container>
         <ImageCont>
           <Image>
-            <img className="feedImage" />
+            <img src={image2} className="feedImage" />
           </Image>
         </ImageCont>
-        <Info>house info</Info>
+        <Info>
+          <h5>Burbank near Studios - quiet, safe neighborhood</h5>
+          2 Guests
+          <br />
+          1 Bedroom
+          <br />
+          1 bed
+          <br />
+          1.5 Shared baths
+          <br />
+        </Info>
       </Container>
       <Container>
         <ImageCont>
           <Image>
-            <img className="feedImage" />
+            <img src={image3} className="feedImage" />
           </Image>
         </ImageCont>
-        <Info>house info</Info>
+        <Info>
+          {" "}
+          <h5>Charming Guests Suite in Hancock Park, 2BD,Pool</h5>
+          4 Guests
+          <br />
+          2 Bedrooms
+          <br />
+          3 beds
+          <br />
+          1 private bath
+          <br />
+        </Info>
       </Container>
       <Container>
         <ImageCont>
           <Image>
-            <img className="feedImage" />
+            <img src={image4} className="feedImage" />
           </Image>
         </ImageCont>
         <Info>house info</Info>
@@ -85,7 +110,7 @@ const DashboardCard = props => {
       <Container>
         <ImageCont>
           <div>
-            <img className="feedImage" />
+            <img src={image5} className="feedImage" />
           </div>
         </ImageCont>
         <Info>house info</Info>
