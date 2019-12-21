ca2af849d071cca05b861df9e84543c39ef7b07a
Jared Brown <jared@Jareds-MBP.lan1>
2019-12-20 23:13:57 -0800

    added first popular house on dashboard

--- airbnbopt/src/components/DashboardCard.js
+++ airbnbopt/src/components/DashboardCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import image1 from "../images/Airbnb1.jpg";
 
 const Container = styled.div`
   height: 200px;
@@ -9,34 +10,85 @@ const Container = styled.div`
   justify-content: center;
 `;
 
+const ImageCont = styled.div`
+  width: 275px;
+  align-text: center;
+`;
+
+const Image = styled.div`
+  height: 180px;
+  width: 255px;
+`;
+
+const Info = styled.div`
+  width: 275px;
+`;
+
 const DashboardCard = props => {
   return (
     <div className="">
-      {/* <img 
+      {/* <<div>
+         <img 
                 src={props}
                 alt="pictures of Most popular homes in LA"
                 ></img>
-          <Title>{props}</Title>*/}
+            <Title>{props}</Title>
+        </div>>*/}
 
       <Container>
-        <div>img</div>
-        <div>house info</div>
+        <ImageCont>
+          <Image>
+            <img
+              src={image1}
+              className="feedImage"
+              alt="photo by: https://www.airbnb.com/users/show/30303945"
+            />
+          </Image>
+        </ImageCont>
+        <Info>
+          {" "}
+          <h5>LA Pool, Privacy and Amazing Views!</h5>
+          3 guests
+          <br />
+          1 bedroom
+          <br />
+          2 beds
+          <br />
+          1.5 baths
+          <br />
+        </Info>
       </Container>
       <Container>
-        <div>img</div>
-        <div>house info</div>
+        <ImageCont>
+          <Image>
+            <img className="feedImage" />
+          </Image>
+        </ImageCont>
+        <Info>house info</Info>
       </Container>
       <Container>
-        <div>img</div>
-        <div>house info</div>
+        <ImageCont>
+          <Image>
+            <img className="feedImage" />
+          </Image>
+        </ImageCont>
+        <Info>house info</Info>
       </Container>
       <Container>
-        <div>img</div>
-        <div>house info</div>
+        <ImageCont>
+          <Image>
+            <img className="feedImage" />
+          </Image>
+        </ImageCont>
+        <Info>house info</Info>
       </Container>
       <Container>
-        <div>img</div>
-        <div>house info</div>
+        <ImageCont>
+          <div>
+            <img className="feedImage" />
+          </div>
+        </ImageCont>
+        <Info>house info</Info>
       </Container>
     </div>
   );
