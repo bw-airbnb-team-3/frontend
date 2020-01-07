181f54c9c2b3f374eea25cd463d64eb644599e5d
Jared Brown <jared@Jareds-MBP.lan1>
2020-01-06 21:00:43 -0800

    styled nav-bar

--- airbnbopt/src/components/header.js
+++ airbnbopt/src/components/header.js
@@ -3,39 +3,38 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
-
-display: flex;
-justify-content: space-between;
-align-items: center;
-background: #fff;
-box-shadow: 0 30px 60px 0 rgba(0,0,0,0.2);
-
+  background: linear-gradient(to right, #414345, #232526);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  box-shadow: 0 30px 60px 0 rgba(0, 0, 0, 0.2);
 `;
 
 const Logo = styled.div`
-padding: 0px 0px 0px 50px;
- 
+  padding: 0px 0px 0px 50px;
+  color: gray;
+  font-family: "Bungee Inline", cursive;
 `;
 
 const Nav = styled.nav`
-display: flex;
-
+  display: flex;
 `;
 
 const NavLinks = styled.a`
   text-decoration: none;
-  color: black;
+  color: gray;
   padding: 0px 50px 0px 0px;
   font-weight: 500;
+  font-family: "Bungee Inline", cursive;
 `;
 
 export default Header => {
   return (
-    <Wrapper>
+    <Wrapper className="nav-container">
       <Logo>
         <h2>Logo Here</h2>
       </Logo>
-      <Nav>
+      <Nav className="nav-bar">
         <NavLinks href="#">Home</NavLinks>
         <NavLinks href="#">About</NavLinks>
         <NavLinks href="#">Login</NavLinks>
