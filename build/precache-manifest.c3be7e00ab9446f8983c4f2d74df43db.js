e754af299aad1e5e39a89b2e40af8e10643da513
Jared Brown <jaredbrown1@hotmail.com>
2020-01-13 01:47:20 -0800

    moved files out of airbnb opt and deleted folder for hosting purposes

---
+++ build/precache-manifest.c3be7e00ab9446f8983c4f2d74df43db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1cbe52290b57226710b11a6beed445f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8933c650d210dfb5830",
+    "url": "/static/css/main.142c9d49.chunk.css"
+  },
+  {
+    "revision": "343a9fc85cd1010a4f34",
+    "url": "/static/js/2.c02e8a8e.chunk.js"
+  },
+  {
+    "revision": "0c559e1ae0a9426ead1485b442d61f0b",
+    "url": "/static/js/2.c02e8a8e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c8933c650d210dfb5830",
+    "url": "/static/js/main.c45091f1.chunk.js"
+  },
+  {
+    "revision": "df744734a4a8887b2f15",
+    "url": "/static/js/runtime-main.2d37078e.js"
+  },
+  {
+    "revision": "3054dc56027f484d0857ff6fb6210267",
+    "url": "/static/media/Airbnb1.3054dc56.jpg"
+  }
+]);
